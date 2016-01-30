2a387b28cf4d9defc335e7ffb707f2844b132bfa
Iván Alejandro <ivanali@outlook.com>
2016-01-30 03:57:42 -0600

    Basic functionality using socket.io done. Missing event handler

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var loader = require('./libs/loader.js');
 var alphabet = require('./libs/alphabet.js');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
@@ -13,10 +15,25 @@ app.get('/', function(request, response) {
     words = [];
     for (var i = 0; i < 5; i++) words.push(loader.getRandomWord());
     // console.log(words.length);
-    console.log(alphabet.letters);
+    // console.log(alphabet.letters);
     response.render('game', {words: words, letters: alphabet.letters});
 });
 
-app.listen(3000, function() {
-    console.log("Express");
+io.on('connection', function(socket) {
+    // send the alphabet to every user that gets connected (this is the full alphabet)
+    io.emit('alphabet update', alphabet.letters);
+    console.log('User connected');
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
+    socket.on('key add', function(key) {
+        console.log('key entered:' + key);
+        var alphabet = alphabet.reduceAlphabet(key, alphabet.letters);
+        console.log(alphabet);
+        // io.emit('alphabet update', alphabet);
+    });
+});
+
+http.listen(3000, function() {
+    console.log("Express running on port 3000");
 });
--- libs/alphabet.js
+++ libs/alphabet.js
@@ -2,3 +2,8 @@ exports.letters = ['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'e',
 'f', 'f', 'g', 'g', 'h', 'h', 'i', 'i', 'i', 'j', 'j', 'k', 'k', 'l', 'l',
 'm', 'm', 'n', 'n', 'o', 'o', 'o', 'p', 'p', 'p', 'q', 'q', 'r', 'r', 's',
 's', 't', 't', 'u', 'u', 'u', 'v', 'v', 'w', 'w', 'x', 'x', 'y', 'y', 'z', 'z'];
+
+exports.reduceAlphabet = function(key, arr) {
+    arr.splice(arr.indexOf(key), 1);
+    return arr;
+}
